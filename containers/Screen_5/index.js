0a6584c3739033f7839d7f46d83e1528d0a6db9b
karimahmedshalabi <karimahmedshalabi@gmail.com>
2018-06-13 05:05:54 +0200

    Finish screen 5

--- containers/Screen_5/index.js
+++ containers/Screen_5/index.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 import { Dimensions, View, Text, StatusBar, Platform, TextInput, KeyboardAvoidingView } from 'react-native'
 import { Button } from 'native-base'
 import { LinearGradient } from 'expo';
-import { FontAwesome } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 import { mainColor } from '../../constants/Colors'
+import { inputBorderRadius, buttonBorderRadius } from '../../constants/gStyles';
 
 const width = Dimensions.get('window').width
 const height = Dimensions.get('window').height
@@ -38,7 +39,7 @@ export default class Screen extends Component {
 	render () {
 		return (
 			<LinearGradient 
-				colors={['#e9bc76', '#f2817b']}
+				colors={['#19d7fb', '#1e63ee']}
 				start={{ x: 0.0, y: 1.0 }}
 				end={{ x: 1.0, y: 0.0 }}
 				style={{
@@ -48,39 +49,29 @@ export default class Screen extends Component {
 					alignItems: 'center'
 				}}>
 				<View style={{ position: 'absolute', marginTop: height * 0.15, alignItems: 'center' }}>
-					<FontAwesome 
-						name='shopping-basket' color='white' size={55} />
+					<MaterialIcons 
+						name='store' color='white' size={55} />
 
-					<Text style={{ color: 'white', fontSize: 43, fontWeight: 'bold' }}>INGE</Text>
-					<Text style={{ color: '#EEEEEE', fontSize: 20 }}>e-Commerce Shop UI App Template</Text>
+					<Text style={{ color: 'white', fontSize: 43, fontWeight: 'bold' }}>AWESOME</Text>
+					<Text style={{ color: '#EEEEEE', fontSize: 20 }}>e-Commerce Shop Template</Text>
 				</View>
 				
 				<View style= {{
 					alignItems: 'center',
 					position: 'absolute',
 					marginTop: height * 0.56596701649175412293853073463268,
-					width: width * 0.85,
+					width: width * 0.85
 				}}>
-					<KeyboardAvoidingView 
-						behavior='position'
-						keyboardVerticalOffset={60}
-						style={{ 
-							backgroundColor: 'white',
-							borderRadius: 1,
-							padding: width * 0.03,
-							width: '100%'
-						}}>
-						<TextInput
-							onChangeText={email => this.setState({ email })}
-							placeholder='Your Email'
-							placeholderTextColor='#afafaf'
-							underlineColorAndroid='transparent'
-							style={{ backgroundColor: '#eeeeee', width: '100%', paddingLeft: 8, marginBottom: width * 0.03 }} />
+					<TextInput
+						onChangeText={email => this.setState({ email })}
+						placeholder='Your Email'
+						placeholderTextColor='#afafaf'
+						underlineColorAndroid='transparent'
+						style={{ backgroundColor: '#eeeeee', width: '100%', paddingLeft: 8, marginBottom: width * 0.03, borderRadius: inputBorderRadius }} />
 
-						<Button block style={{ elevation: 0, backgroundColor: mainColor }} onPress={this.validate} >
-							<Text style={{ color: 'white'}}>SEND</Text>
-						</Button>
-					</KeyboardAvoidingView>
+					<Button block style={{ elevation: 0, backgroundColor: mainColor, borderRadius: buttonBorderRadius }} onPress={this.validate} >
+						<Text style={{ color: 'white'}}>SEND</Text>
+					</Button>
 				</View>
 			</LinearGradient>
 		)
