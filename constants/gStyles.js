9b618566c2c91c3e0d185917abd0ca9e29b613a5
karimahmedshalabi <karimahmedshalabi@gmail.com>
2018-06-13 03:53:36 +0200

    Add inputBorderRadius to gStyles

--- constants/gStyles.js
+++ constants/gStyles.js
@@ -1,7 +1,9 @@
 export const buttonBorderRadius = 20
 export const boxBorderRadius = 2
+export const inputBorderRadius = 3
 
 export default {
 	buttonBorderRadius, 
-	boxBorderRadius
+	boxBorderRadius,
+	inputBorderRadius
 }
