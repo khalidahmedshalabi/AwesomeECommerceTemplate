c01a86ae9a70c103811c3eb38159cf2a8af9648d
ZizoHussien <36141034+ZizoHussien@users.noreply.github.com>
2018-06-13 08:53:55 +0200

    Custom component

---
+++ Component/ProductCard/index.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { Text, View, FlatList } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import Stars from 'react-native-stars-rating'
+import { mainColor } from '../../constants/Colors';
+export default class ProductCard extends React.Component {
+	render() {
+		return (
+			const priceTag = <Text style={{ fontSize: 13, color: mainColor }}>{this.props.currentPrice}$</Text>
+			const priceContainer = this.props.oldPrice === 0 ? priceTag : (
+				<View style={{ flexDirection: 'column' }}>
+					<Text style={{ fontSize: 13, color: '#969696', textDecorationLine: 'line-through', marginRight: 3 }}>{this.props.oldPrice}$</Text>
+					{priceTag}
+				</View>
+				<View style={{ flex: 1, flexDirection: 'row', borderRadius: 1 }}>
+					<View style={{ flex: 0.6, backgroundColor: '#c8c8c8' }}>
+					</View>
+					<View style={{ flex: 1, backgroundColor: 'white', paddingHorizontal: 13, paddingVertical: 9, justifyContent: 'center' }}>
+						<View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+							<Text style={{ fontWeight: 'bold' }}>{this.props.title}</Text>
+							{
+								(this.props.badgeColor && this.props.badgeText) ? <Text style={{
+									color: 'white',
+									backgroundColor: this.props.badgeColor,
+									padding: 4,
+									fontSize: 12
+								}}>{this.props.badgeText}</Text> : null
+							}
+						</View>
+						<Stars
+							isActive={false}
+							rateMax={5}
+							isHalfStarEnabled={false}
+							onStarPress={(rating) => console.log(rating)}
+							rate={this.props.stars}
+							color='#f9e784'
+							size={13}
+						/>
+						<Text style={{ color: '#969696', textAlign: 'justify', paddingVertical: 8 }}>{this.props.description}</Text>
+								<View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end'}}>
+									{priceContainer}
+									<View style={{ flexDirection: 'row' }}>
+										<View style={{ backgroundColor: 'white',borderColor:mainColor,borderWidth:1.2,paddingVertical:3,paddingHorizontal:9, borderRadius:4, marginRight: 7 }}>
+											<FontAwesome name='heart' color='#5ac8fa' size={18} />
+										</View>
+										<View style={{ flexDirection: 'column', alignItems: 'center', backgroundColor: 'white',borderColor:mainColor,borderWidth:1.2,paddingVertical:3, paddingHorizontal:9, borderRadius:4}}>
+											<FontAwesome name='cart-plus' color='#5ac8fa' size={20} />
+										</View>
+									</View>
+								</View>
+					</View>
+				</View>
+			)
+		}
+		)
+	}
+}
--- containers/Screen_18/RelatedItems.js
+++ containers/Screen_18/RelatedItems.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import { Text, View, FlatList } from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 import Stars from 'react-native-stars-rating'
 import { mainColor } from '../../constants/Colors';
-
+import ProductCard from '../../Component/ProductCard/index';
 export default class Reviews extends Component {
 	constructor() {
 		super()
@@ -64,74 +65,25 @@ export default class Reviews extends Component {
 		}
 	}
 
-	renderProduct = (item) => {
-		const priceTag = <Text style={{ fontSize: 13, color: mainColor }}>{item.currentPrice}$</Text>
-		const priceContainer = item.oldPrice === 0 ? priceTag : (
-			<View style={{ flexDirection: 'row' }}>
-				<Text style={{ fontSize: 13, color: '#969696', textDecorationLine: 'line-through', marginRight: 3 }}>{item.oldPrice}$</Text>
-				{priceTag}
-			</View>
-		)
 
-		return (
-			<View style={{ flex: 1, flexDirection: 'row', borderRadius: 1 }}>
-				<View style={{ flex: 0.6, backgroundColor: '#c8c8c8' }}>
-
-				</View>
-
-				<View style={{ flex: 1, backgroundColor: 'white', paddingHorizontal: 13, paddingVertical: 9, justifyContent: 'center' }}>
-					<View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-						<Text style={{ fontWeight: 'bold' }}>{item.title}</Text>
-
-						{
-							(item.badgeColor && item.badgeText) ? <Text style={{
-								color: 'white',
-								backgroundColor: item.badgeColor,
-								padding: 4,
-								fontSize: 12
-							}}>{item.badgeText}</Text> : null
-						}
-					</View>
-
-					<Stars
-						isActive={false}
-						rateMax={5}
-						isHalfStarEnabled={false}
-						onStarPress={(rating) => console.log(rating)}
-						rate={item.stars}
-						color='#f9e784'
-						size={13}
-					/>
-
-					<Text style={{ color: '#969696', textAlign: 'justify', paddingVertical: 8 }}>{item.description}</Text>
-
-					<View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end' }}>
-						{priceContainer}
-						<View style={{ flexDirection: 'row' }}>
-							<View style={{ backgroundColor: mainColor, padding: 6, borderRadius: 1, marginRight: 3 }}>
-								<Ionicons name='ios-heart-outline' color='white' size={18} />
-							</View>
-
-							<View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: mainColor, padding: 6, borderRadius: 1 }}>
-								<Ionicons name='md-add' color='white' size={14} />
-								<Text style={{ color: 'white', fontSize: 12, marginLeft: 3 }}>ADD TO CART</Text>
-							</View>
-						</View>
-					</View>
-				</View>
-			</View>
-		)
-	}
 
 	render() {
 		return (
+			<View style={{flex:1,backgroundColor:'white'}}>
 			<FlatList
-				data={this.state.products}
-				style={{ flex: 1 }}
-				contentContainerStyle={{ padding: 14 }}
-				ItemSeparatorComponent={() => <View style={{ backgroundColor: 'transparent', height: 14 }}></View>}
-				renderItem={({ item }) => this.renderProduct(item)}
-			/>
+		style={{ flex:1 }}
+		data = {this.state.products}
+		contentContainerStyle={{ paddingVertical: 14 }}
+		ItemSeparatorComponent={() => <View style={{ backgroundColor: '#dedede', height: 1 }}></View>}
+		renderItem = {({ item }) => (
+			  <ProductCard navigation={this.props.navigation}
+			  title= item.title
+			  description= item.description
+			  currentPrice= item.currentPrice
+			  OldPrice= item.oldPrice
+			  Stars= item.Stars />
+		  )} />
+			</View>
 		)
 	}
 }
