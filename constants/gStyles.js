3b56946607c674fd4634112b9cd32ed0e0978f71
karimahmedshalabi <karimahmedshalabi@gmail.com>
2018-06-14 03:03:58 +0200

    Add imageBorderRadius to gStyles

--- constants/gStyles.js
+++ constants/gStyles.js
@@ -1,9 +1,11 @@
 export const buttonBorderRadius = 20
 export const boxBorderRadius = 2
 export const inputBorderRadius = 3
+export const imageBorderRadius = 10
 
 export default {
 	buttonBorderRadius, 
 	boxBorderRadius,
-	inputBorderRadius
+	inputBorderRadius,
+	imageBorderRadius
 }
