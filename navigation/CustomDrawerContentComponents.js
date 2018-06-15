ad7f15955cbdeb5a4443f612e687ae18352dfd1e
karimahmedshalabi <karimahmedshalabi@gmail.com>
2018-06-15 02:12:59 +0200

    Finish screen 9

--- navigation/CustomDrawerContentComponents.js
+++ navigation/CustomDrawerContentComponents.js
@@ -2,9 +2,7 @@ import React from 'react';
 import {
 	ScrollView,
 	View,
-	Image,
 	Text,
-	ImageBackground,
 	TouchableOpacity,
 } from 'react-native'
 import { LinearGradient } from 'expo';
@@ -16,15 +14,15 @@ export const CustomDrawer1ContentComponent = (props) => {
 		<ScrollView style={{ backgroundColor: '#fff', flex: 1, }}>
 			<TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', paddingLeft: 16, paddingVertical: 28 }} onPress={() => {}} >
 				<View style={{ width: 70, height: 70, borderRadius: 35, backgroundColor: '#c8c8c8', borderColor: mainColor, borderWidth: 1 }}></View>
-				<View style={{ flexDirection: 'column', margin: 20, }}>
-					<Text style={{ fontSize: 18, color: 'black', fontWeight: 'bold' }}>Samantha Jones</Text>
-					<Text style={{ fontSize: 16, color: '#969696',  }}>Seattle, USA</Text>
+				<View style={{ flexDirection: 'column', margin: 20}}>
+					<Text style={{ fontSize: 18, color: 'black', fontWeight: 'bold' }}>John Bartl</Text>
+					<Text style={{ fontSize: 16, color: '#969696',  }}>London, UK</Text>
 				</View>
 			</TouchableOpacity>
 
 			<DrawerItems 
 				{...props}
-				style={{ paddingLeft: 10 }} />
+				style={{ paddingLeft: 10}} />
 		</ScrollView>
 	);
 }
@@ -45,8 +43,8 @@ export const CustomDrawer2ContentComponent = (props) => {
 				<TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', paddingLeft: 16, paddingVertical: 28 }} onPress={() => { }} >
 					<View style={{ width: 70, height: 70, borderRadius: 35, backgroundColor: '#c8c8c8', borderColor: 'white', borderWidth: 1 }}></View>
 					<View style={{ flexDirection: 'column', margin: 20, }}>
-						<Text style={{ fontSize: 18, color: 'white', fontWeight: 'bold' }}>Samantha Jones</Text>
-						<Text style={{ fontSize: 16, color: '#fcdbd5', }}>Seattle, USA</Text>
+						<Text style={{ fontSize: 18, color: 'white', fontWeight: 'bold' }}>John Bartl</Text>
+						<Text style={{ fontSize: 16, color: '#fcdbd5', }}>London, UK</Text>
 					</View>
 				</TouchableOpacity>
 
